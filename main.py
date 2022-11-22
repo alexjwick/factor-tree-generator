63cd1f42f278abd302ef95a29e746876f97b4e23
alexjwick <alexjwick03@gmail.com>
2022-11-22 14:24:00 -0500

    tree height calculated correctly

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 import math
 
+treeHeight: int = 0
+
 class Node:
-    def __init__(self, val: int, level: int):
+    def __init__(self, val: int, depth: int):
         self.left: Node = None
         self.right: Node = None
         self.val: int = val
-        self.level: int = level
+        self.depth: int = depth
 
 def generateFactorTree(node: Node) -> Node:
     node: Node = node
+
+    global treeHeight
+
     if node.val == 0:
+        treeHeight = 1
         return node
     elif node.val < 0:
-        node.left = Node(-1, node.level + 1)
-        node.right = generateFactorTree(Node(-1 * num, node.level + 1))
+        node.left = Node(-1, node.depth + 1)
+        node.right = generateFactorTree(Node(-1 * num, node.depth + 1))
         return node
     else:
-        for i in range(2, int(math.sqrt(node.val))):
+        for i in range(2, int(node.val/2)):
             if node.val % i == 0:
-                node.left = Node(i, node.level + 1)
-                node.right = generateFactorTree(Node(node.val / i, node.level + 1))
+                node.left = Node(i, node.depth + 1)
+                node.right = generateFactorTree(Node(int(node.val / i), node.depth + 1))
                 return node
+        treeHeight = node.depth + 1
         return node
+
+def printFactorTree(root: Node) -> None:
+    root: Node = root
 
 if __name__ == '__main__':
     while True:
@@ -33,7 +43,9 @@ if __name__ == '__main__':
             except ValueError:
                 isInt = False
             if isInt:
-                generateFactorTree(Node(num, 0))
+                root: Node = generateFactorTree(Node(num, 0))
+                print(treeHeight)
+                treeHeight = 0
             else:
                 print("Input must be an integer")
         else:
